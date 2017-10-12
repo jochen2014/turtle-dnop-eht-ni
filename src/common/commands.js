035610af5036dc5d5c7b9bacf5efec53736b9153
Bo Chen <bochen2014@yahoo.com>
2017-10-13 01:22:20 +1100

    command ok

--- src/common/commands.js
+++ src/common/commands.js
@@ -1,13 +1,25 @@
-const commands = [/^place\s+\d,\s*\d,\s*(north|east|south|west)$/i, 'move', 'left', 'right', 'report'];
-const isValidCommand = command => {
-    return !!commands.find(c => {
+const commands = [/^place\s+(\d)\s*,\s*(\d)\s*,\s*(north|east|south|west)$/i, 'MOVE', 'LEFT', 'RIGHT', 'REPORT'];
+const parseCommand = command => {
+    let parsedResult;
+    commands.some(c => {
         if (typeof c === 'object') { //regex;
-            return c.test(command);
-        } else {
-            return command.toLowerCase() === c;
+            const match = command.match(c);
+            if (match) {
+                parsedResult = {
+                    command: 'PLACE',
+                    args: match.slice(1, 4)
+                }
+                return true; //found! now exit;
+            }
+        } else { //string
+            if (command.toUpperCase() === c) {
+                parsedResult = { command: c };
+                return true; //found! now exit;
+            }
         }
     });
+    return parsedResult;
 }
 export {
-    isValidCommand,
+    parseCommand,
 }
--- src/components/homePage.js
+++ src/components/homePage.js
@@ -1,14 +1,27 @@
 import React, { Component } from 'react';
-import { isValidCommand } from '../common/commands';
+import { parseCommand } from '../common/commands';
 
 const directions = ['NORTH', 'EAST', 'SOUTH', 'WEST'];
+const keyMap = [{
+    key: 'ArrowLeft',
+    command: 'LEFT'
+}, {
+    key: 'ArrowUp',
+    command: 'MOVE'
+}, {
+    key: 'ArrowRight',
+    command: 'RIGHT'
+}, {
+    key: 'ArrowDown',
+    command: 'REPORT'
+}]
 class HomePage extends Component {
 
     constructor() {
         super();
         this.state = {
             command: '',
-            f: directions[0],
+            f: 'NORTH',
             x: 0,
             y: 0,
             reports: [],
@@ -36,16 +49,34 @@ class HomePage extends Component {
         }, 500);
     }
 
-    handleKeyDown = e => {
+    executeCommand = (_command, args) => {
+        //place 0, 0, north
+        console.log(`---------  executing command ${_command} with args ${args}`);
+        const command = _command.toUpperCase();
         const { x, y, f: facing } = this.state;
         const directionIndex = directions.indexOf(facing);
-        switch (event.keyCode) {
-            case 37: //left;
+        switch (command) {
+            case 'PLACE': {
+                const [x, y, f] = args;
+                if (x > 4 || y > 4) {
+                    this.addReport('cannot go outside the pen');
+                    return;
+                }
+                console.log(`---------  set facing => ${f.toUpperCase()}`);
+                this.setState({
+                    x: Number(x),
+                    y: Number(y),
+                    f: f.toUpperCase(),
+                }, ()=>{
+                    console.log(` ==========  ${this.state.f}`);
+                })
+            }
+            case 'LEFT':
                 this.setState({
                     f: directions[(directionIndex + 3) % 4]
                 });
                 break;
-            case 38: //up
+            case 'MOVE':
                 switch (directionIndex) {
                     case 0: //north
                         if (y === 4) {
@@ -85,17 +116,25 @@ class HomePage extends Component {
                         break;
                 }
                 break;
-            case 39: //right;
+            case 'RIGHT':
                 this.setState({
                     f: directions[(directionIndex + 1) % 4]
                 });
                 break;
-            case 40: //down;
+            case 'REPORT':
                 this.addReport(`${x},${y},${facing}`);
                 break;
             default:
                 break;
         }
+    }
+
+    handleKeyDown = e => {
+        const result = keyMap.find(m => m.key === e.key);
+        if (result) {
+            this.executeCommand(result.command);
+        }
+
     }
 
     onCellClicked = (x, y) => e => {
@@ -119,7 +158,9 @@ class HomePage extends Component {
             command: value
         })
 
+
     }
+
     onCommandKeyPressed = e => {
         if (e.key === 'Enter') {
             this.onExecuteCommand();
@@ -130,8 +171,10 @@ class HomePage extends Component {
         if (!command) {
             return;
         }
-        if (isValidCommand(command)) {
-
+        const parsedCommand = parseCommand(command)
+        if (parsedCommand) {
+            const { command, args } = parsedCommand;
+            this.executeCommand(command, args);
         } else {
             this.addReport(`invalid command: ${command}`);
         }
@@ -141,7 +184,8 @@ class HomePage extends Component {
     }
 
     render() {
-        const { command, reports } = this.state;
+        const { command, reports , f} = this.state;
+        console.log(`---------  facing ${f}`);
         return <div className="container">
             <div className="title">
                 <h1>Turtle in the Pond</h1>
