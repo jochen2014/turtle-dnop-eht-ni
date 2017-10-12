cb4a575ee81d35eb74cfb57bb627617e56d6cc4f
Bo Chen <bochen2014@yahoo.com>
2017-10-12 23:58:31 +1100

    key listening down

--- src/components/homePage.js
+++ src/components/homePage.js
@@ -1,13 +1,100 @@
 import React, { Component } from 'react';
 
+const directions = ['NORTH', 'EAST', 'SOUTH', 'WEST'];
 class HomePage extends Component {
+
     constructor() {
         super();
         this.state = {
+            f: directions[0],
             x: 0,
             y: 0,
+            reports: [],
         }
     }
+
+    componentWillMount() {
+        document.addEventListener("keydown", this.handleKeyDown);
+    }
+
+    componentWillUnmount() {
+        // NOTE:When the page refreshes react doesn't have the chance to unmount the components as normal. 
+        // Use the window.onbeforeunload event to set a handler for refresh
+        document.removeEventListener("keydown", this.handleKeyDown);
+    }
+
+    addReport = newReport => {
+        const {reports} = this.state;
+        const newReports = [...reports].concat([newReport]).slice(-5); //we keep 5 maximum logs;
+        const self = this;
+        setTimeout(function () {
+            self.setState({
+                reports: newReports,
+            })
+        }, 500);
+    }
+    handleKeyDown = e => {
+        const { x, y, f: facing } = this.state;
+        const directionIndex = directions.indexOf(facing);
+        switch (event.keyCode) {
+            case 37: //left;
+                this.setState({
+                    f: directions[(directionIndex + 3) % 4]
+                });
+                break;
+            case 38: //up
+                switch (directionIndex) {
+                    case 0: //north
+                        if (y === 4) {
+                            this.addReport('cannot go north')
+                            return false;
+                        }
+                        this.setState({
+                            y:y+1,
+                        })
+                        break;
+                    case 1: //east
+                        if (x === 4) {
+                            this.addReport('cannot go east')
+                            return false;
+                        }
+                        this.setState({
+                           x:x+1,
+                        })
+                        break;
+                    case 2: //south
+                        if (y === 0) {
+                            this.addReport('cannot go south')
+                            return false;
+                        }
+                        this.setState({
+                            y:y-1,
+                         })
+                        break;
+                    case 3: //west
+                        if (x === 0) {
+                            this.addReport('cannot go west')
+                            return false;
+                        }
+                        this.setState({
+                            x:x-1,
+                         })
+                        break;
+                }
+                break;
+            case 39: //right;
+                this.setState({
+                    f: directions[(directionIndex + 1) % 4]
+                });
+                break;
+            case 40: //down;
+                this.addReport(`${x},${y},${facing}`);
+                break;
+            default:
+                break;
+        }
+    }
+
     onCellClicked = (x, y) => e => {
         this.setState({
             x,
@@ -15,9 +102,9 @@ class HomePage extends Component {
         })
     }
     getCellClassName = (x, y) => {
-        const { x: selectedX, y: selectedY } = this.state;
+        const { x: selectedX, y: selectedY, f } = this.state;
         if (x === selectedX && y === selectedY) {
-            return 'selected'
+            return 'selected-' + f.toLowerCase()[0];
         }
         return '';
     }
@@ -31,7 +118,7 @@ class HomePage extends Component {
                 <input type="text" />
                 <button>Go</button>
             </div>
-            <div className="canvas">
+            <div className="canvas" >
                 {
                     Array.from({ length: 5 }).map((e, rowIndex) => (
                         < div className="row" key={`row_${rowIndex}`}>
@@ -43,6 +130,13 @@ class HomePage extends Component {
                                         onClick={this.onCellClicked(columnIndex, 4 - rowIndex)} />))
                             }
                         </div>
+                    ))
+                }
+            </div>
+            <div className="log">
+                {
+                    this.state.reports.map((r, index) => (
+                        <div key={`key_${index}`}>{r}</div>
                     ))
                 }
             </div>
